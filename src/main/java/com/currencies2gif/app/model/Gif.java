7d49a31491f4e77996030acca09ea2612e5467b7
Rustykik <valentin.dubr@gmail.com>
2022-06-03 21:20:55 +0300

    add model
    added gif model to return it in json format
    also added Gif parser but:
    parser should throw custom exceptions if we have invalid json from gif api
    need to add more tests including on throwing exceptions

---
+++ src/main/java/com/currencies2gif/app/model/Gif.java
@@ -0,0 +1,10 @@
+package com.currencies2gif.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+public class Gif {
+    private final String url;
+}
---
+++ src/main/java/com/currencies2gif/app/utils/GifParser.java
@@ -0,0 +1,26 @@
+package com.currencies2gif.app.utils;
+
+import org.json.JSONObject;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GifParser {
+
+    public static String getUrlFromRawJSON(String json) {
+        JSONObject obj = new JSONObject(json);
+        String gifUrl = obj.getJSONObject("data")
+                .getJSONObject("images")
+                .getJSONObject("original")
+                .getString("url");
+        return gifUrl;
+    }
+    // TODO add exception
+    public static String parseIdFromUrl(String url) {
+        final String regex = "(?<=/media/)(.*)(?=/giphy)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(url);
+        matcher.find();
+        return matcher.group(0);
+    }
+}
