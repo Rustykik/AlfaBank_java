ff6c5ee11279453e52d7ec81747c098f2c3e64a1
Rustykik <valentin.dubr@gmail.com>
2022-06-04 01:10:23 +0300

    rename test name

---
+++ src/test/java/com/currencies2gif/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencies2gif.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/currencies2gif/app/service/UserServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.currencies2gif.app.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+
+    @Test
+    void getYesterdayRates() {
+    }
+
+    @Test
+    void getLatestRates() {
+    }
+
+    @Test
+    void getGif() {
+    }
+}
