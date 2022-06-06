6c648867ce26d32e08ffdb89717c3a5a118688f5
Rustykik <valentin.dubr@gmail.com>
2022-06-06 16:11:44 +0300

    add test to service
    better exception handling testing of service

--- src/test/java/com/currencies2gif/app/service/UserServiceImplTest.java
+++ src/test/java/com/currencies2gif/app/service/UserServiceImplTest.java
@@ -4,6 +4,7 @@ import com.currencies2gif.app.exception.ApiRequestCurrencyException;
 import com.currencies2gif.app.exception.ThirdPartyApiInvalidAnswerException;
 import com.currencies2gif.app.feignclients.FeignCurrencyClient;
 import com.currencies2gif.app.feignclients.FeignGifClient;
+import com.currencies2gif.app.model.Gif;
 import feign.FeignException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -11,7 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -21,57 +25,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserServiceImplTest {
-
-
-    /*
-    {
-  "disclaimer": "Usage subject to terms: https://openexchangerates.org/terms",
-  "license": "https://openexchangerates.org/license",
-  "timestamp": 1654451788,
-  "base": "USD",
-  "rates": {}
-}
-{
-  "disclaimer": "Usage subject to terms: https://openexchangerates.org/terms",
-  "license": "https://openexchangerates.org/license",
-  "timestamp": 1654451788,
-  "base": "USD",
-  "rates": {
-    "BTC": 0.000033287503
-  }
-}
-{
-  "error": true,
-  "status": 403,
-  "message": "not_allowed",
-  "description": "Changing the API `base` currency is available for Developer, Enterprise and Unlimited plan clients. Please upgrade, or contact support@openexchangerates.org with any questions."
-}
-{
-  "error": true,
-  "status": 401,
-  "message": "invalid_app_id",
-  "description": "Invalid App ID provided. Please sign up at https://openexchangerates.org/signup, or contact support@openexchangerates.org."
-}
-https://openexchangerates.org/api/latest.json?base=RUB&app_id=3009f7558066428f83561e07c8c8309c&symbols=BT
-     */
-    String CORRECT_CURRENCY_JSON = "{\n" +
-            "  \"disclaimer\": \"Usage subject to terms: https://openexchangerates.org/terms\",\n" +
-            "  \"license\": \"https://openexchangerates.org/license\",\n" +
-            "  \"timestamp\": 1654451788,\n" +
-            "  \"base\": \"USD\",\n" +
-            "  \"rates\": {\n" +
-            "    \"BTC\": 0.000033287503\n" +
-            "  }\n" +
-            "}";
-    String WRONG_CURRENCY_JSON = "{\n" +
-            "  \"error\": true,\n" +
-            "  \"status\": 401,\n" +
-            "  \"message\": \"invalid_app_id\",\n" +
-            "  \"description\": \"Invalid App ID provided. Please sign up at https://openexchangerates.org/signup, or contact support@openexchangerates.org.\"\n" +
-            "}";
-
-    String CORRECT_GIF_JSON = "cor";
-    String WRONG_GIF_JSON = "wrg";
 
     @Autowired
     UserService underTest;
@@ -88,7 +41,8 @@ https://openexchangerates.org/api/latest.json?base=RUB&app_id=3009f7558066428f83
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String currTime = LocalDate.now().minusDays(1).format(formatter);
         String symbol = "BTC";
-        Mockito.when(mockFeignCurrencyClient.getCurrenciesOnDate(currTime, "BTC")).thenReturn(CORRECT_CURRENCY_JSON);
+        Mockito.when(mockFeignCurrencyClient.getCurrenciesOnDate(currTime, symbol))
+                .thenReturn(ServiceTestSetup.CURRENCY_VALID_200);
         //when
         BigDecimal currency = underTest.getYesterdayRates(symbol);
         //then
@@ -97,12 +51,55 @@ https://openexchangerates.org/api/latest.json?base=RUB&app_id=3009f7558066428f83
     }
 
     @Test
+    void getYesterdayRates_Get_InvalidRequest_And_Should_ReturnApiRequestCurrencyException() {
+        //given
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String currTime = LocalDate.now().minusDays(1).format(formatter);
+        String symbol = "BTC";
+        Mockito.when(mockFeignCurrencyClient.getCurrenciesOnDate(currTime, symbol))
+                .thenReturn(ServiceTestSetup.INVALID_CURRENCY_REQUEST_200);
+        //when
+
+        //then
+        assertThatThrownBy(()->underTest.getYesterdayRates(symbol)).isInstanceOf(ApiRequestCurrencyException.class);
+
+    }
+
+    @Test
+    void getYesterdayRates_Get_401_And_Should_ReturnThirdPartyApiInvalidAnswerExcept() {
+        //given
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String currTime = LocalDate.now().minusDays(1).format(formatter);
+        String symbol = "BTC";
+        Mockito.when(mockFeignCurrencyClient.getCurrenciesOnDate(currTime, symbol))
+                .thenThrow(FeignException.Forbidden.class);
+        //when
+
+        //then
+        assertThatThrownBy(()->underTest.getYesterdayRates(symbol)).isInstanceOf(ThirdPartyApiInvalidAnswerException.class);
+
+    }
+
+    @Test
+    void getYesterdayRates_Get_403_And_Should_ReturnThirdPartyApiInvalidAnswerExcept() {
+        //given
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String currTime = LocalDate.now().minusDays(1).format(formatter);
+        String symbol = "BTC";
+        Mockito.when(mockFeignCurrencyClient.getCurrenciesOnDate(currTime, symbol))
+                .thenThrow(FeignException.Unauthorized.class);
+        //when
+
+        //then
+        assertThatThrownBy(()->underTest.getYesterdayRates(symbol)).isInstanceOf(ThirdPartyApiInvalidAnswerException.class);
+    }
+    @Test
     void getYesterdayRates_Should_Throw_ApiRequestCurrencyException() {
         //given
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String currTime = LocalDate.now().minusDays(1).format(formatter);
         String symbol = "BTC";
-        Mockito.when(mockFeignCurrencyClient.getCurrenciesOnDate(currTime, "BTC")).thenThrow(FeignException.class);
+        Mockito.when(mockFeignCurrencyClient.getCurrenciesOnDate(currTime, symbol)).thenThrow(FeignException.class);
         //when
 
         //then
@@ -114,8 +111,8 @@ https://openexchangerates.org/api/latest.json?base=RUB&app_id=3009f7558066428f83
         //given
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String currTime = LocalDate.now().minusDays(1).format(formatter);
-        String symbol = "USD";
-        Mockito.when(mockFeignCurrencyClient.getCurrenciesOnDate(currTime, "USD")).thenThrow(FeignException.class);
+        String symbol = "BTC";
+        Mockito.when(mockFeignCurrencyClient.getCurrenciesOnDate(currTime, symbol)).thenThrow(FeignException.class);
         //when
 
         //then
@@ -126,7 +123,7 @@ https://openexchangerates.org/api/latest.json?base=RUB&app_id=3009f7558066428f83
     void getLatest_Should_Get_Rates() {
         //given
         String symbol = "BTC";
-        Mockito.when(mockFeignCurrencyClient.getLatestCurrencies("BTC")).thenReturn(CORRECT_CURRENCY_JSON);
+        Mockito.when(mockFeignCurrencyClient.getLatestCurrencies(symbol)).thenReturn(ServiceTestSetup.CURRENCY_VALID_200);
         //when
         BigDecimal currency = underTest.getLatestRates(symbol);
         //then
@@ -134,28 +131,79 @@ https://openexchangerates.org/api/latest.json?base=RUB&app_id=3009f7558066428f83
     }
 
     @Test
-    void getLatest_Should_Throw_ApiRequestCurrencyException() {
+    void getLatestRates_Get_InvalidRequest_And_Should_ReturnApiRequestCurrencyException() {
         //given
-        String symbol = "USD";
-        Mockito.when(mockFeignCurrencyClient.getLatestCurrencies("USD")).thenThrow(FeignException.class);
+        String symbol = "BTC";
+        Mockito.when(mockFeignCurrencyClient.getLatestCurrencies( symbol))
+                .thenReturn(ServiceTestSetup.INVALID_CURRENCY_REQUEST_200);
         //when
 
         //then
-        assertThatThrownBy(() -> underTest.getYesterdayRates(symbol)).isInstanceOf(ApiRequestCurrencyException.class);
+        assertThatThrownBy(()->underTest.getLatestRates(symbol)).isInstanceOf(ApiRequestCurrencyException.class);
+
     }
 
     @Test
-    void getLatest_Should_Throw_ThirdPartyApiInvalidAnswerException() {
+    void getLatestRates_Get_401_And_Should_ReturnThirdPartyApiInvalidAnswerExcept() {
         //given
-        String symbol = "USD";
-        Mockito.when(mockFeignCurrencyClient.getLatestCurrencies("USD")).thenThrow(FeignException.class);
+        String symbol = "BTC";
+        Mockito.when(mockFeignCurrencyClient.getLatestCurrencies(symbol))
+                .thenThrow(FeignException.Forbidden.class);
         //when
 
         //then
-        assertThatThrownBy(() -> underTest.getYesterdayRates(symbol)).isInstanceOf(ThirdPartyApiInvalidAnswerException.class);
+        assertThatThrownBy(()->underTest.getLatestRates(symbol)).isInstanceOf(ThirdPartyApiInvalidAnswerException.class);
+
     }
 
     @Test
-    void getGif() {
+    void getLatestRates_Get_403_And_Should_ReturnThirdPartyApiInvalidAnswerExcept() {
+        //given
+        String symbol = "BTC";
+        Mockito.when(mockFeignCurrencyClient.getLatestCurrencies(symbol))
+                .thenThrow(FeignException.Unauthorized.class);
+        //when
+
+        //then
+        assertThatThrownBy(()->underTest.getLatestRates(symbol)).isInstanceOf(ThirdPartyApiInvalidAnswerException.class);
     }
+
+    @Test
+    void getGif_ShouldWorkCorrect() {
+        //given
+        String tag = "rich";
+        Mockito.when(mockFeignGifClient.getRandomGif(tag)).thenReturn(ServiceTestSetup.GIF_FOUND_200);
+
+        //when
+        Gif toCheck = underTest.getGif(tag);
+
+        //then
+        assertThat(toCheck.getUrl()).isNotNull();
+    }
+
+    @Test
+    void getGif_ShouldWorkCorrect2() {
+        //given
+        String tag = "rich";
+        Mockito.when(mockFeignGifClient.getRandomGif(tag)).thenReturn(ServiceTestSetup.GIF_FOUND1_200);
+
+        //when
+        Gif toCheck = underTest.getGif(tag);
+
+        //then
+        assertThat(toCheck.getUrl()).isNotNull();
+    }
+
+    @Test
+    void getGif_ShouldThrowException() {
+        //given
+        String tag = "rich";
+        Mockito.when(mockFeignGifClient.getRandomGif(tag)).thenReturn(ServiceTestSetup.GIF_NOT_FOUND);
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> underTest.getGif(tag)).isInstanceOf(ThirdPartyApiInvalidAnswerException.class);
+    }
+
 }
